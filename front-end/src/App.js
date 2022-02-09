885c2abf89758d021a7f1f9185d4f9f080dbd760
jiaqichen6688 <jiaqi.c.chen@accenture.com>
2022-02-09 16:57:35 +0000

    updated code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react';
 
-function App() {
+
+const RequestButton = (props) => {
+  return <button onClick={props.onClick}> Press me {props.message}</button>
+} 
+
+/*const WelcomeMessage = (message) => {
+  //var message = "";
+  return <div>{message}</div>
+}*/
+
+function App()  {
+
+  const [message, setMessage] = useState("no message");
+
+  //useEffect(() => {onClick()}, []);
+
+
+  async function onClick(){
+    console.log("the button has been clicked woooo!")
+    const res = await fetch('http://localhost:3001')
+    const data = await res.text()
+    console.log(data)
+    setMessage(data);
+    //(data);
+   // return data
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <RequestButton onClick={onClick} message={message}> </RequestButton>
+    
+    </div>)
+  
 }
 
+
+
+
+
 export default App;
+
