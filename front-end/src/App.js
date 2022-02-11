bb9f1bbc95092e564a008b8bbca3e10e9a44f502
jiaqichen6688 <jiaqi.c.chen@accenture.com>
2022-02-11 16:41:11 +0000

    updated code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,47 @@
+import * as React from 'react'
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState} from 'react';
+import { useEffect, useState} from 'react'
+import {
+  BrowserRouter ,
+  Switch,
+  Route,
+  NavLink,
+  useParams,
+  useLocation,
+  useHistory,
+  Link
+} from "react-router-dom";
+
+const Routes = () => (
+  <Switch>
+    <Route path="/mybooking">
+      <h1>Booking</h1>
+    </Route>
+    <Route path="/venues">
+      <h1>View Venue</h1>
+    </Route>
+    <Route path="/">
+      <h1>Home</h1>
+    </Route>
+  </Switch>
+);
+
+const NavBar = () => {
+  // For illustration
+  const { pathname } = useLocation();
+
+  return (
+    <>
+      <label>{pathname}</label>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/venues">View venue</NavLink>
+        <NavLink to="/mybooking">Booking</NavLink>
+      </nav>
+    </>
+  );
+};
 
 
 const RequestButton = (props) => {
@@ -21,7 +62,7 @@ function App()  {
 
   async function onClick(){
     console.log("the button has been clicked woooo!")
-    const res = await fetch('http://localhost:3001')
+    const res = await fetch('https://kdm665d8y1.execute-api.eu-west-2.amazonaws.com/production/')
     const data = await res.text()
     console.log(data)
     setMessage(data);
@@ -30,10 +71,13 @@ function App()  {
    
   }
   return (
-    <div>
-    <RequestButton onClick={onClick}>  </RequestButton>
-    <p>{message}</p>
-    </div>)
+    <><BrowserRouter>
+      <NavBar />
+      <Routes />
+    </BrowserRouter><div>
+        <RequestButton onClick={onClick}>  </RequestButton>
+        <p>{message}</p>
+      </div></>)
   
 }
 
