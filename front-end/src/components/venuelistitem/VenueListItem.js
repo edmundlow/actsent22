95bfee3058e1a28277a954de2783046bd85d8128
Lucy Mullins <lucy.mullins@accenture.com>
2022-02-22 17:02:12 +0000

    working venue list view  with boostrap

--- front-end/src/components/venuelistitem/VenueListItem.js
+++ front-end/src/components/venuelistitem/VenueListItem.js
@@ -3,16 +3,20 @@
 const VenueListItem = (props) =>{
     return (
         <div key={props.key}>
-            <div class="card mb-3">
-                <div class="card-body">
-                    <img class="card-img-top" src={props.image} alt="Card image cap"/>
-                    <h4 class="card-title">{props.name}</h4>
-                    <p class="card-text">
-                        {props.location}
-                        <br/>
-                        {props.description}
-                    </p>
+
+            <div class="card mb-3" >
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                <img src={props.image} class="card-img" alt="..."/>
                 </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{props.name}</h5>
+                    <p class="card-text">{props.location} <br/>
+                        {props.description}</p>
+                </div>
+                </div>
+            </div>
             </div>
         </div>
     )
