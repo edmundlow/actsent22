839c9730e473aa6db6602268cf1c8e0ab744d794
Lucy Mullins <lucy.mullins@accenture.com>
2022-02-24 14:47:35 +0000

    test for react component venuelistitem

--- front-end/src/components/venuelistitem/VenueListItem.js
+++ front-end/src/components/venuelistitem/VenueListItem.js
@@ -4,15 +4,15 @@ const VenueListItem = (props) =>{
     return (
         <div key={props.key}>
 
-            <div class="card mb-3" >
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                <img src={props.image} class="card-img" alt="..."/>
+            <div className="card mb-3" >
+            <div className="row no-gutters">
+                <div className="col-md-4">
+                <img src={props.image} className="card-img" alt="..."/>
                 </div>
-                <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">{props.name}</h5>
-                    <p class="card-text">{props.location} <br/>
+                <div className="col-md-8">
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text">{props.location} <br/>
                         {props.description}</p>
                         <ViewVenuePage />
                 </div>
