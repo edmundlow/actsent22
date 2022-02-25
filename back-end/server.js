2ad430f1c0ce5565f8fbacf02e807ba9a39d0807
Mariola Rubio <mariola.rubio.canton@accenture.com>
2022-02-25 16:05:12 +0000

    Created event POST and GET endpoints, and event/status PUT endpoint.

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-// const postgres = require('pg-promise')()
+const postgres = require('pg-promise')()
 const app = express()
 
-// const db = postgres({
-//  host: '127.0.0.1',
-//  database: 'actsent',
-//  user: 'postgres',
-//  password: 'mysecretpassword'
-// })
+// To-Do: find out DB name
+const db = postgres({
+ host: process.env.DB_HOSTNAME || '127.0.0.1',
+ database: 'gigstr',
+ user: 'gigstr',
+ password: process.env.DB_PASSWORD || 'gigstr',
+})
 
 // app.use(cors())
 // app.get('/', async (req, res) => {
@@ -47,10 +48,41 @@ app.get('/venue', (req, res) => {
       description : "Legends only"
     },
 
-
-
-
   ])
+})
+
+
+app.post('/create_event', (req, res) => {
+  const venue_id = req.body.id
+  const venue_name = req.body.location
+  const date = req.body.date
+  const event_name = req.body.name
+  const event_description= req.body.description
+  const event_image = req.body.image
+  const artist_name = req.body.artist_name
+  const artist_email=req.body.artist_email
+  const genre=req.body.genre
+  const status=req.body.status
+  db.query(
+    'INSERT INTO events(venue_id, venue, date, event_name,  event_description, event_image,artist_name,artist_email,genre,status) VALUES ($1, $2, $3, $4, $5, $6, $7, $8 ,$9) RETURNING event_id;',
+    [venue_id, venue_name, date, event_name, event_description, event_description, event_image, artist_name, artist_email, genre, status])
+    .then(res.status(200).send('Event creation successful'))
+})
+
+app.get('/events', async (req,res) => {
+  const events = await db.query('SELECT * FROM event ;')
+  res.send(events)
+})
+
+app.put('/event', (req,res) => {
+  
+})
+
+app.put('/event/status', (req,res) => {
+  const new_status = req.body.status
+  const event_id = req.body.id
+  db.query(`UPDATE event SET status = ${new_status} WHERE id = ${event_id} `)
+  .then(res.status(200).send('Status updated successfully'))
 })
 
 module.exports = app
