155c290c39788dbaf5acb240ea06dce5ee4489e2
Lucy Mullins <lucy.mullins@accenture.com>
2022-03-02 15:42:45 +0000

    Co-authored-by: jiaqichen6688 <jiaqichen6688@users.noreply.github.com> Co-authored-by: Beshay, Marco <marco.beshay@accenture.com> Co-authored-by: mariolarubio <mariolarubio@users.noreply.github.com>

--- front-end/src/pages/EventsPage.js
+++ front-end/src/pages/EventsPage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState} from 'react'
 
-const event_id = 1
+//const event_id = 1
 
 async function fetchEvents(event_id, artist_email){
     const res = await fetch('http://localhost:3002/events')
@@ -8,48 +8,64 @@ async function fetchEvents(event_id, artist_email){
     const data = await res.json()
     // data.filter(events => events.artistEmail === artistEmail)
     console.log("data", data)
-    const event = data.find(events => events.event_id === event_id)// && events.artist_email === artist_email)    
+    console.log("event id", event_id)
+    //const event = data.find(events => events.event_id === event_id)// && events.artist_email === artist_email)    
     
-    console.log(event)
-    return event
+    //console.log(event)
+    return data.find(events => events.event_id === event_id)// && events.artist_email === artist_email)
 }
 
+
+
 const SearchEventsForm = (props) =>{
-
-    const [eventId, setEventId] = useState('')
+    const [submitBtnState, setSubmitBtnState] = useState(true)
+    const [eventId, setEventId] = useState(0)
     const [artistEmail, setArtistEmail] = useState('')
-
+    const [event, setEvent] = useState({})
+    
+    useEffect(() => {
+        fetchEvents(parseInt(eventId), artistEmail).then(setEvent)
+    }, [submitBtnState])
+    
     const onSubmit = (e) =>{
         // TO PREVENT THE PAGE RELOAD ON SUBMIT
         e.preventDefault()
+        
+       // const event = fetchEvents(eventId, artistEmail).then(setEvent)
+        console.log(event)
+        
+        // return <EventsPage event={event}/>
 
-        const event = fetchEvents(artistEmail, eventId)
-
-        return <EventsPage event={event}/>
+        setSubmitBtnState(!submitBtnState)
     
     }
 
     return (
+        <>
 
-    <form onSubmit={onSubmit}>
+            <form onSubmit={onSubmit}>
 
-           <div>
-               <label>Enter your event id </label>
-               <input type='number' placeholder="1" 
-                value={eventId} onChange={(e)=> {setEventId(e.target.value)}}/>
+                <div>
+                    <label>Enter your event id </label>
+                    <input type='number' placeholder="1" 
+                    value={eventId} onChange={(e)=> {setEventId(e.target.value)}}/>
 
-           </div>
+                </div>
 
-           <div>
-               <label>Enter your email </label>
-               <input type='text' placeholder="1" 
-                value={artistEmail} onChange={(e)=> {setArtistEmail(e.target.value)}}/>
+                <div>
+                    <label>Enter your email </label>
+                    <input type='text' placeholder="1" 
+                    value={artistEmail} onChange={(e)=> {setArtistEmail(e.target.value)}}/>
                 
-           </div>
+                </div>
 
-           <input type='submit' value='Find Event' />
+                <input type='submit' value='Find Event' />
 
-       </form>
+            </form>
+
+        <EventsPage event={event}/>
+        
+    </>
     )
     
 }
@@ -67,7 +83,9 @@ const  EventsPage = (props) =>{
 
     return (
         <>
-        
+        {
+        props.event === undefined ? <p> please wait ..</p> : 
+        <>
             <h1>{props.event.event_name}</h1>
             <h4>{props.event.event_description}</h4>
             <h4>{props.event.artist_name}</h4>
@@ -75,6 +93,8 @@ const  EventsPage = (props) =>{
             <h4> {props.event.genre} </h4>
             <h4>{props.event.status} </h4>
 
+</>
+}
         </>
     )
 }
